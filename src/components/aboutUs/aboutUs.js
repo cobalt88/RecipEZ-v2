38f57546d5c8188ee9436b9f2445e106420a6f74
Vincent Teune <50896378+cobalt88@users.noreply.github.com>
2022-08-19 12:24:44 -0400

    2.0.0-aboutUs.2
    rough outline of about us page
    
    vince 8.19.22

---
+++ src/components/aboutUs/aboutUs.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+return (
+  <>
+    <header className="aboutUs-header">
+      <h1>About RecipEZ</h1>
+    </header>
+    <main>
+      <h2>What Is It?</h2>
+      <p>
+        RecipEZ is the last recipe resource you will ever need. <br />
+        Recipes First. Stories Second. Thats our core philosophy. <br />
+        A one stop shop for finding, sharing, and storing all your recipes.{" "}
+        <br />
+        A social network focussed on something every human needs - Food. <br />
+        Future add-ins: recipe matcher based on ingredients you have, recipe
+        suggestions, sharable recipe collections. <br />
+      </p>
+      <h2>What It Isn't</h2>
+      <p>
+        RecipEZ IS NOT a food blog. Theres too man of those already. <br />
+      </p>
+      <div>
+        <h2>Meet the people that made it happen</h2>
+        {/* collaborator/contributor cards go here. */}
+      </div>
+    </main>
+  </>
+);
