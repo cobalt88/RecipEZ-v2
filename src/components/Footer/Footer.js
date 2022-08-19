e49e44d3d7528813dcbc6b1b958d8a54133e2671
Vincent Teune <50896378+cobalt88@users.noreply.github.com>
2022-08-19 13:01:32 -0400

    2.0.0-footer
    initial setup of page footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+return (
+  <>
+    <div>
+      {/* links to about us, contact us, legal information, all the other miscellaneous stuff that goes on the bottom of a page */}
+    </div>
+  </>
+);
