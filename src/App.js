0c9449defa8188a9391128561c55766e046fd2c7
Vincent Teune <50896378+cobalt88@users.noreply.github.com>
2022-08-19 12:57:49 -0400

    2.0.0-develop.3
    merge in landing page and begin importing components to App.js
    
    vince 8.19.22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Nav } from "./components/Nav";
+import { LandingPage } from "./components/LandingPage";
+import { AboutUs } from "./components/AboutUs";
+import { LoginPage } from "./components/LoginPage";
+import { RecipeCard } from "./components/RecipeCard";
+import { SignUpPage } from "./components/SignUpPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Nav></Nav>;
 }
 
 export default App;
